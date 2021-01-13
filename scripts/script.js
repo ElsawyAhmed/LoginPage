0145c060df715acdbb6ce07611004c4e649257ea
ElsawyAhmed <77357350+ElsawyAhmed@users.noreply.github.com>
2021-01-14 00:25:28 +0200

    Hi , I am uploading my file
    This is the first time here , things may seem to be trivial to you but it means a lot to me

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+var users = [], userN, userP, userM;
+var user = { userN, userP, userM };
+user = {
+    userN: "Ahmed", userP: "123", userM: "elsawyahmed@gmail.com"
+};
+users.push(user);
+
+    function authenticate() {
+        var currn = document.getElementById("name").value;
+        var currp = document.getElementById("pass").value;
+        var i;
+
+        for (i = 0; i < users.length; i++) {
+            if (currn == users[i].userN) {
+                if (currp == users[i].userP)
+                    document.getElementById("msg").innerHTML = "Successful login >> Your Mail is : </br>" + users[i].userM + " ";
+                else
+                    document.getElementById("msg").innerHTML = "Failed to login Wrong Password";
+
+            }
+            else
+                document.getElementById("msg").innerHTML = "Failed to login Wrong User";
+
+        }
+}
+function add() {
+    var currn = document.getElementById("name").value;
+    var currp = document.getElementById("pass").value;
+    var currm = document.getElementById("email").value;
+    var newuser = {
+        userN: currn, userP: currp, userM: currm
+    }
+    users.push(newuser);
+    console.log(users.length);
+    document.getElementById("msg").innerHTML = "User Added Successfully ";
+
+}
